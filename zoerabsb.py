ba0e279631b0a20f45b734368bd3eff18a1db278
THEREALSET <seltonluiz007@gmail.com>
2026-05-20 22:22:25 -0300

    Delete

--- zoerabsb.py
+++
@@ -1,5 +0,0 @@
-val = str(input('A minha vea é muitcho linda? [S/N] ')).upper().strip()[0]
-if val == 'S':
-    print('Você está certo , ela é muito linda msm :) ')
-else:
-    print('Vá para um oftamologista. ')
